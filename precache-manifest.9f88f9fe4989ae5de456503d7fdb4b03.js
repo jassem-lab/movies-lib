1793d3c0b5c7ccb087dfc544e5e897c93a927c92
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2020-11-23 00:46:05 +0100

    Updates

---
+++ precache-manifest.9f88f9fe4989ae5de456503d7fdb4b03.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d17069f541231ca21b42",
+    "url": "/movies-lib/static/js/main.71d45ec1.chunk.js"
+  },
+  {
+    "revision": "2c1a49cf0d16b95f1bc0",
+    "url": "/movies-lib/static/js/runtime~main.09059fd3.js"
+  },
+  {
+    "revision": "f2a68cbc885f10f47b96",
+    "url": "/movies-lib/static/css/2.d28afc14.chunk.css"
+  },
+  {
+    "revision": "f2a68cbc885f10f47b96",
+    "url": "/movies-lib/static/js/2.04683627.chunk.js"
+  },
+  {
+    "revision": "4c58037b84e2b796d404d3cb2770a210",
+    "url": "/movies-lib/static/media/nothing.4c58037b.svg"
+  },
+  {
+    "revision": "fdbc4613a1ea54685aefe7e130011d3c",
+    "url": "/movies-lib/static/media/person.fdbc4613.svg"
+  },
+  {
+    "revision": "bcc287eaf67b81a8f6332e2dff80eb16",
+    "url": "/movies-lib/static/media/logo.bcc287ea.svg"
+  },
+  {
+    "revision": "0f4a7f7476ed84b3527c3f993de82a9c",
+    "url": "/movies-lib/static/media/tmdb.0f4a7f74.svg"
+  },
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/movies-lib/static/media/tmdbgreen.27b65cb4.svg"
+  },
+  {
+    "revision": "e0b5f43f549eaaae96de6476830468fa",
+    "url": "/movies-lib/static/media/empty.e0b5f43f.svg"
+  },
+  {
+    "revision": "cbc8724a992b15ec4f477b412ec14120",
+    "url": "/movies-lib/static/media/error.cbc8724a.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/movies-lib/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/movies-lib/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/movies-lib/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/movies-lib/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "a7a39ce1e151f2a1f246343672ccc168",
+    "url": "/movies-lib/index.html"
+  }
+];
